d50cd5ec6a0551a41fc8a1836a90e28067a834cb
Banbis <victorhugomlaker9@gmail.com>
2024-02-02 15:35:59 -0300

    Desafio da Calculadora Concluida!

---
+++ desafio.js
@@ -0,0 +1,45 @@
+let saldoVictory = calculaVitoria(500, 200);
+let nivel;
+
+function calculaVitoria(vitoria, derrota)
+{
+    saldoVitorias = vitoria - derrota
+    return saldoVitorias
+}
+
+if(saldoVitorias < 10)
+{
+    nivel = "Ferro"
+}
+
+else if (saldoVitorias >= 11 && saldoVitorias <= 20)
+{
+    nivel = "Bronze"
+}
+
+else if (saldoVitorias >= 21 && saldoVitorias <= 50)
+{
+    nivel = "Prata"
+}
+
+else if (saldoVitorias >= 51 && saldoVitorias <= 80)
+{
+    nivel = "Ouro"
+}
+
+else if (saldoVitorias >= 81 && saldoVitorias <= 90)
+{
+    nivel = "Diamante"
+}
+
+else if (saldoVitorias >= 91 && saldoVitorias <= 100)
+{
+    nivel = "Lendário"
+}
+
+else 
+{
+    nivel = "Imortal"
+}
+
+console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`)
